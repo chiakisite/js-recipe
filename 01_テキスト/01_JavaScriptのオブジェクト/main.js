0e35e23a336612bca620fc2b7bc5f1681e475298
chiakisite <chiakik0522@gmail.com>
2021-09-29 22:24:19 +0900

    オブジェクトまで終了！

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -5,12 +5,49 @@ const hikakin = {
   isHappy: true,
   address: {
     country: "日本",
-    city: "東京"
+    city: "東京",
   },
   sayHello: function() {
     console.log("ブンブンハロー YouTube")
-  }
+  },
 }
 
+console.log(hikakin)
+
+const wakanya = {
+  name: "松本わかな",
+  age: 14,
+}
+
+console.log(wakanya)
+
+const kawamu = {
+  name: "川村文乃",
+  age: 22,
+  fishCanCook: ["まぐろ", "かつお", "うなぎ", "他にもいろいろ！"],
+  ishappy: true,
+  syussin: {
+    pref: "高知県",
+    city: "高知市",
+  },
+  hougen: function() {
+    console.log("キラキラしちゅう")
+  },
+}
+
+console.log(kawamu)
+console.log(wakanya.name)
+console.log(wakanya["age"])
+
+const readerJun = {
+  0: "あやか",
+  1: "あかり",
+}
+console.log(readerJun[1])
+const Name = "name"
+console.log(kawamu[Name])
+console.log(kawamu.name)
+kawamu.hougen()
 // 練習問題
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+hikakin.sayHello()
