007416fbcb7f3dad2cf9d0397e961fed2fca4438
chiakisite <chiakik0522@gmail.com>
2021-10-15 20:55:14 +0900

    選択肢の数とボタンの表示

--- 02_レシピ/発展問題/02_クイズ（１問）/main.js
+++ 02_レシピ/発展問題/02_クイズ（１問）/main.js
@@ -2,22 +2,21 @@ const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
 const feedback = document.getElementById("feedback")
 
-const words = ["ゴリアテ", "ゼニガメ", "ガニメデ"]
-
-const buttonmaker = function(n, text) {
+const button = function(n) {
   const newButton = document.createElement("button")
-  const buttonText = document.createTextNode(text)
-  newButton.appendChild(buttonText)
-  newButton.setAttribute("id", `button-${n}`)
-  const choicesContainer = document.getElementById("choices-container")
+  let newText
   if (n === 0) {
-    choicesContainer.insertBefore(newButton, choicesContainer.firstChild)
-    return document.getElementById(`button-${n}`)
-  } else {
-    const before = document.getElementById(`button-${n - 1}`)
-    choicesContainer.insertBefore(newButton, before.nextSibling)
-    return document.getElementById(`button-${n}`)
+    newText = document.createTextNode("ゴリアテ")
+  } else if (n === 1) {
+    newText = document.createTextNode("ゼニガメ")
+  } else if (n === 2) {
+    newText = document.createTextNode("ガニメデ")
   }
+  newButton.appendChild(newText)
+  newButton.setAttribute("id", `choice-${n}`)
+  const choices = document.getElementById("choices-container")
+  choices.appendChild(newButton)
+  return document.getElementById(`choice-${n}`)
 }
 
 //クイズの内容
@@ -26,19 +25,20 @@ const quiz = {
   image: "Ganymede.jpg",
   choices: [
     {
-      text: words[0],
-      button: buttonmaker(0, quiz.choices[0].text),
-      feedback: `残念！${words[0]}は、旧約聖書に登場するダビデに石で殺される巨人だよ。`,
+      text: "ゴリアテ",
+      feedback:
+        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ",
+      button: button(0),
     },
     {
-      text: words[1],
-      button: buttonmaker(1, quiz.choices[1].text),
-      feedback: `残念！${words[1]}は、クサガメまたはニホンイシガメの幼体だよ。`,
+      text: "ゼニガメ",
+      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      button: button(1),
     },
     {
-      text: words[2],
-      button: buttonmaker(2, quiz.choices[2].text),
-      feedback: `正解！${words[2]}は、木星の第三惑星だよ！`,
+      text: "ガニメデ",
+      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      button: button(2),
     },
   ],
 }
@@ -50,12 +50,11 @@ const reloadQuiz = function() {
 
   //画像
   quizImage.src = "./images/" + quiz.image
+
+  //選択肢
 }
 
-//選択肢
-const choice1 = quiz.choices[0].button
-const choice2 = quiz.choices[1].button
-const choice3 = quiz.choices[2].button
+reloadQuiz()
 
 //選択する処理
 const choose = function(choiceString) {
@@ -63,16 +62,14 @@ const choose = function(choiceString) {
   feedback.textContent = quiz.choices[choiceString].feedback
 } //選択する処理
 
-choice1.onclick = function() {
+quiz.choices[0].button.onclick = function() {
   choose(0)
 }
 
-choice2.onclick = function() {
+quiz.choices[1].button.onclick = function() {
   choose(1)
 }
 
-choice3.onclick = function() {
+quiz.choices[2].button.onclick = function() {
   choose(2)
 }
-
-reloadQuiz()
