a1266e58f59f7add161cd60e26b9e6afe69c96fc
chiakisite <chiakik0522@gmail.com>
2021-10-17 15:51:05 +0900

    Trello(発展)

--- 02_レシピ/発展問題/複数リスト/02_Trello（保存機能なし）/main.js
+++ 02_レシピ/発展問題/複数リスト/02_Trello（保存機能なし）/main.js
@@ -1,142 +1,64 @@
-for (let i = 0; i < 3; i++){
-  const inputToDo[i] = document.getElementById(`input-todo${i}`)
-  const cardsContainer[i] = document.getElementById(`cards-container${i}`)
-  const addButton[i] = document.getElementById(`add-button${i}`)
-  const mainContainer[i] = document.getElementById(`main-container${i}`)
-}
+const inputElement = document.getElementById("input-todo")
+const inputElement1 = document.getElementById("input-todo1")
+const inputElement2 = document.getElementById("input-todo2")
+const container = document.getElementById("cards-container")
+const container1 = document.getElementById("cards-container1")
+const container2 = document.getElementById("cards-container2")
+const addButton = document.getElementById("add-button")
+const addButton1 = document.getElementById("add-button1")
+const addButton2 = document.getElementById("add-button2")
 
+// 追加ボタンを押したときの処理を登録
 
-addButton.onclick = function(n) {
-  const card = createCard(inputToDo[n].value)
-  cardsContainer.append(card)
+addButton.onclick = function() {
+  // テキストをとりだす
+  const card = createCard(inputElement.value)
+  container.append(card)
 
+  // 入力欄を空にする
   inputElement.value = ""
 }
 
+// 追加ボタンを押したときの処理を登録
+addButton1.onclick = function() {
+  // テキストをとりだす
+  const card = createCard(inputElement1.value)
+  container1.append(card)
+
+  // 入力欄を空にする
+  inputElement1.value = ""
+}
+
+// 追加ボタンを押したときの処理を登録
+addButton2.onclick = function() {
+  // テキストをとりだす
+  const card = createCard(inputElement2.value)
+  container2.append(card)
+
+  // 入力欄を空にする
+  inputElement2.value = ""
+}
 
 const createCard = function(text) {
+  // カードを追加する
   const card = document.createElement("div")
   card.className = "card"
 
+  // テキストを表示する部分を作る
   const todo = document.createElement("div")
   todo.className = "todo"
   todo.textContent = text
   card.append(todo)
 
+  // 削除ボタンを作る
   const deleteButton = document.createElement("div")
   deleteButton.className = "delete"
 
-  cardsContainer.append(card)
-  card.append(todo)
-  card.append(deleteButton)
-
+  // 削除ボタンを押したときの処理を登録
   deleteButton.onclick = function() {
+    // カードを削除する
     card.remove()
   }
+  card.append(deleteButton)
   return card
-}
-
-
-for (let i = 0; i < 3; i++){
-  const addCategory = document.getElementById(`add-category${i}`)
-}
-
-
-
-
-let number = 1
-
-const newCreateCard = function() {
-  const listContainer = document.createElement("div")
-  listContainer.className = "list-container"
-  listContainer.id = "list-container"
-  mainContainer.append(listContainer)
-
-  const header = document.createElement("div")
-  header.className = "list-header"
-  header.id = "list-header"
-  header.textContent = number += 1
-  listContainer.append(header)
-
-  const cardsContainer = document.createElement("div")
-  cardsContainer.className = "cards-container"
-  cardsContainer.id = "cards-container"
-  header.after(cardsContainer)
-
-  const listFooter = document.createElement("div")
-  listFooter.className = "list-footer"
-  listFooter.id = "list-footer"
-  cardsContainer.after(listFooter)
-
-  const inputContainer = document.createElement("div")
-  inputContainer.className = "input-container"
-  inputContainer.id = "input-container"
-  listFooter.append(inputContainer)
-
-  const inputTodo = document.createElement("input")
-  inputTodo.className = "input-todo"
-  inputTodo.id = "input-todo"
-  inputContainer.append(inputTodo)
-
-  const inputButton = document.createElement("div")
-  inputButton.className = "input-button"
-  inputButton.id = "add-button"
-  inputButton.textContent = "追加"
-  inputTodo.after(inputButton)
-
-  const category = document.createElement("div")
-  category.className = "category"
-  category.id = "category"
-  listFooter.after(category)
-
-  const addCategory = document.createElement("div")
-  addCategory.className = "add-category"
-  addCategory.id = "add-category"
-  addCategory.textContent = "＋カードを追加"
-  category.append(addCategory)
-
-  inputButton.onclick = function() {
-    const card = createCard(inputTodo.value)
-    cardsContainer.append(card)
-
-    inputTodo.value = ""
-  }
-  //Enterキーで追加可能処理
-  inputTodo.onkeydown = function(event) {
-    if (event.key === "Enter") {
-      const card = createCard(inputTodo.value)
-      cardsContainer.append(card)
-
-      inputTodo.value = ""
-    }
-  }
-
-  const createCard = function(text) {
-    const card = document.createElement("div")
-    card.className = "card"
-
-    const todo = document.createElement("div")
-    todo.className = "todo"
-    todo.textContent = text
-
-    const deleteButton = document.createElement("div")
-    deleteButton.className = "delete"
-
-    cardsContainer.append(card)
-    card.append(todo)
-    card.append(deleteButton)
-
-    deleteButton.onclick = function() {
-      card.remove()
-    }
-    return card
-  }
-
-  addCategory.onclick = function() {
-    newCreateCard()
-  }
-}
-
-addCategory.onclick = function() {
-  newCreateCard()
 }
